808db568e03e780eae8fdb4e8239adaece3995c8
Arpanish <apsa_csit2075@lict.edu.np>
2021-05-28 19:07:44 +0545

    templating,view for post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from.views import index
+from.views import index, postlist
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post-list/', postlist, name='post-list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,16 @@ from django.http import HttpResponse
 from .models import Post
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-id")[:3]
     print(posts)
 
    
     return render(request, 'index.html', {'posts': posts})
+
+def postlist(request):
+     posts = Post.objects.all()
+     print(posts)
+     return render(request, 'blog/blog-list.html', {'posts':posts})  
+
 
 # Create your views here.
